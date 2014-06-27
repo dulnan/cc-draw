611dea259974cc729ecdb847e091e531fd5cbc59
Jan Hug <jan.hug@liip.ch>
2014-06-27 09:52:19 +0200

    add functionality to select colors

--- cinema.js
+++ cinema.js
@@ -7,10 +7,23 @@ var CEILING_Y = 4;      // amount of panels on the Y axis
 
 var CURRENT_COLOR = '#000000';
 
+var COLORS = ['#FFFFFF', '#dcdcdc', '#8c8c8c', '#414141', '#000000',
+              '#fdb913', '#f68b28', '#f26531',
+              '#1ab5a8', '#07978b', '#007c77',
+              '#d54361'];
+
 
 $(document).ready(function() {
     initCeiling();
+    createColorTiles();
+    $('.colortile').on('click', function() {
+        CURRENT_COLOR = $(this).data('color');
+        $('.colortile').removeClass('selected');
+        $(this).addClass('selected');
+        console.log('hi');
+    });
 });
+
 
 
 /* initialize the ceiling */
@@ -19,6 +32,7 @@ function initCeiling() {
         createPanel(i);
     }
 }
+
 
 
 /* Creates one panel */
@@ -31,6 +45,8 @@ function createPanel(plid) {
     }
     
 };
+
+
 
 /* Create an LED */
 function createLED(panel, ledID) {
@@ -45,6 +61,19 @@ document.addEventListener('touchmove', function(e) {
     e.preventDefault();
     elem = document.elementFromPoint(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
     if ($(elem).hasClass('led')) {
-        $(elem).css('background-color', 'black');
+        $(elem).css('background-color', CURRENT_COLOR);
     }
 }, false);
+
+
+/* Toolbox */
+function createColorTiles() {
+    COLORS.forEach(function(color) {
+        colortile = $('<div class="colortile"></div>').data('color', color);
+        $(colortile).css('background-color', color);
+        $(colortile).appendTo('#colors');
+    });
+}
+
+
+
